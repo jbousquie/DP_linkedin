0672af77eb783032ce64c838d908602dd356f07d
Jérôme Bousquié <jerome.bousquie@iut-rodez.fr>
2022-11-16 16:56:18 +0100

    iramuteq line

--- scraper_lk.py
+++ scraper_lk.py
@@ -8,17 +8,38 @@
 from bs4 import BeautifulSoup
 
 INPUT_FILE = 'html/LinkedinWagnerJetsPrives.html'
-CSV_FILE = 'output/jets.csv'
+CSV_FILE = 'output/jets_iramuteq.txt'
 
 with open(INPUT_FILE) as input_file:
     data_soup = BeautifulSoup(input_file, 'lxml')
-    #articles = data_soup.find_all('article', class_='comments-comment-item comments-reply-item reply-item')
-    articles = data_soup.find_all('article')
+    articles = data_soup.find_all('article', class_='comments-comment-item')
     for article in articles:
+        post_text = ''
+        reply_count = ''
+        reaction_count = ''
         spantexts = article.find_all('span', class_='comments-comment-item__main-content')
         for spantext in spantexts:
             ltr = spantext.find('span', {"dir": "ltr"})
-            print(ltr.text)
+            if ltr is not None:
+                if post_text == '':
+                    post_text = ltr.text
+                else :
+                    post_text = post_text + '\n' + ltr.text
         
         span_replycount = article.find('span', class_='comments-comment-social-bar__replies-count')
-        print(span_replycount)
+        if span_replycount is not None:
+            reply_count = span_replycount.text.split()[0]
+        
+
+        reaction_button = article.find('button', class_='comments-comment-social-bar__reactions-count' )
+        if reaction_button is not None:
+            reaction_count = reaction_button['aria-label'].split()[0]
+        
+        if reply_count == '':
+            reply_count = '0'
+        
+        if reply_count == '':
+            reaction_button = '0'
+
+        line = '**** *num_1 *reactions_' + reaction_count + ' *comments_' + reply_count + '\n'
+        print(line)
